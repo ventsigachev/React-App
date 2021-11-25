d02cf2b1bfaafb682cbb994c765425fb3d6ab4c9
ventsigachev <ventsigachev@github.com>
2021-11-25 16:50:36 +0200

    Added Create component and functionality. Some Home and Navigation changes.

--- blog/src/components/Create/Create.js
+++ blog/src/components/Create/Create.js
@@ -1,7 +1,74 @@
+import "./Create.css";
+import { useState } from "react";
+import { useNavigate } from 'react-router-dom';
+
+const API_URL = "http://localhost:3030/jsonstore";
+
 const Create = () => {
-    return (
-       <div>Create</div>
-    )
-}
+  const [title, setTitle] = useState("");
+  const [about, setAbout] = useState("");
+  const [description, setDescription] = useState("");
+  const [isPending, setIsPending] = useState(false);
+
+  const navigate = useNavigate();
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    const author = "ven";
+    const likes = 0;
+    const dislikes = 0;
+
+    const story = { title, about, description, author, likes, dislikes };
+
+    setIsPending(true);
+
+    fetch(`${API_URL}/story`, {
+
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(story)
+
+    })
+    .then(() => {
+        setIsPending(false);
+        navigate("/home");
+    })
+  }
+
+  return (
+    <form onSubmit={submitHandler} method="POST">
+      <label htmlFor="title">Your Story Title</label>
+      <input
+        type="text"
+        name="title"
+        id="title"
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
+        required
+      />
+      <label htmlFor="about">Your Story About</label>
+      <input
+        type="text"
+        name="about"
+        id="about"
+        value={about}
+        onChange={(e) => setAbout(e.target.value)}
+        required
+      />
+      <label htmlFor="description">Your Story</label>
+      <textarea
+        type="text"
+        name="description"
+        id="description"
+        value={description}
+        onChange={(e) => setDescription(e.target.value)}
+        placeholder="Write your Story Here..."
+        required
+      />
+      {!isPending && <input type="submit" id="submit" value="Publish" />}
+      {isPending && <input type="submit" id="submit" value="Publishing...." disabled/>}
+    </form>
+  );
+};
 
 export default Create;
--- blog/src/components/Home/Home.js
+++ blog/src/components/Home/Home.js
@@ -1,24 +1,31 @@
 import { useState, useEffect } from "react";
 import Story from "./Story/Story";
+import "./Home.css";
 
-const API_URL = "http://localhost:3030/jsonstore/";
+const API_URL = "http://localhost:3030/jsonstore";
 
 const Home = () => {
   const [story, setStory] = useState([]);
 
   useEffect(() => {
-    fetch(`${API_URL}story/`)
+    fetch(`${API_URL}/story`)
       .then((res) => res.json())
       .then((data) => 
       setStory(Object.values(data)));
   }, []);
 
   return (
+    story.length > 0 ?
     <div className="story-list">
       {story.map((story) => (
         <Story key={story._id} story={story} />
       ))}
+    </div> : 
+    <div className="not-story">
+       <p>There are no stories to display</p>
+        
     </div>
+    
   );
 };
 
