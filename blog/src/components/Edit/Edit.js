0db3918eeda9ed4fbe0bc7f34500d1d56a170739
ventsigachev <ventsigachev@github.com>
2021-11-27 16:59:55 +0200

    Edited Edit view.

--- blog/src/components/Edit/Edit.js
+++ blog/src/components/Edit/Edit.js
@@ -8,7 +8,6 @@ const API_URL = "http://localhost:3030/jsonstore";
 const Edit = () => {
   const { storyId } = useParams();
   const navigate = useNavigate();
-  
 
   const [story, setStory] = useState({});
   const [title, setTitle] = useState("");
@@ -16,22 +15,18 @@ const Edit = () => {
   const [description, setDescription] = useState();
   const [isPending, setIsPending] = useState(false);
 
-
-useEffect(() => {
-    
+  useEffect(() => {
     const getStory = async () => {
-        const res = await fetch(`${API_URL}/story/${storyId}`);
-        const data = await res.json()
-        setStory(data);
-        setTitle(data.title);
-        setAbout(data.about);
-        setDescription(data.description);
-    } 
+      const res = await fetch(`${API_URL}/story/${storyId}`);
+      const data = await res.json();
+      setStory(data);
+      setTitle(data.title);
+      setAbout(data.about);
+      setDescription(data.description);
+    };
 
     getStory();
-      
   }, [storyId]);
-
 
   const submitHandler = (e) => {
     e.preventDefault();
@@ -41,9 +36,16 @@ useEffect(() => {
     const date = moment().format("DD-MM-YYYY HH:mm");
     const _id = storyId;
 
-
-    const story = { _id, title, about, author, date, description, likes, dislikes };
-
+    const story = {
+      _id,
+      title,
+      about,
+      author,
+      date,
+      description,
+      likes,
+      dislikes,
+    };
 
     setIsPending(true);
 
@@ -61,15 +63,18 @@ useEffect(() => {
     <form onSubmit={submitHandler}>
       <label htmlFor="title">Your Story Title</label>
       <input
+        className="edit-text"
         type="text"
         name="title"
         id="title"
         value={title}
         onChange={(e) => setTitle(e.target.value)}
         required
+        autoFocus
       />
       <label htmlFor="about">Your Story About</label>
       <input
+        className="edit-text"
         type="text"
         name="about"
         id="about"
@@ -79,6 +84,7 @@ useEffect(() => {
       />
       <label htmlFor="description">Your Story</label>
       <textarea
+        className="edit-text textarea"
         type="text"
         name="description"
         id="description"
