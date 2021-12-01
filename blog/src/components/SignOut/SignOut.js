ba544fb24823f5ce4fb2bbf3ce66928455a1b41d
ventsigachev <ventsigachev@github.com>
2021-12-01 22:09:19 +0200

    Added Sign Out layout.

--- blog/src/components/SignOut/SignOut.js
+++ blog/src/components/SignOut/SignOut.js
@@ -1,8 +1,20 @@
-import './SignOut.css';
+import "./SignOut.css";
+import { Link } from "react-router-dom";
 
 const SignOut = () => {
   return (
-    <h1>SignOut</h1>
+    <div className="logout">
+      <div className="logoutWrapper">
+        <div className="logoutMessage">
+          <p className="msg">Sorry to see You escape!</p>
+          <p className="msg1">Are You leaving, really?</p>
+        </div>
+        <div className="buttons">
+          <Link to="/" className="bt exit">Leave</Link>
+          <Link to="#" className="bt back">go back</Link>
+        </div>
+      </div>
+    </div>
   );
 };
 
