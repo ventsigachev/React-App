bb22ed8010fcee4b70874c9a6ebb36a0d25141f5
ventsigachev <ventsigachev@github.com>
2021-12-01 13:56:06 +0200

    Added initial register func

--- blog/src/components/SignUp/SignUp.js
+++ blog/src/components/SignUp/SignUp.js
@@ -1,7 +1,27 @@
 import "./SignUp.css";
 import { Link } from "react-router-dom";
+import {useState} from "react";
+
+const REG_API = "http://localhost:3030/users/register";
 
 const SignUp = () => {
+
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+
+  const avatar = "/images/default_pic.jpg";
+
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+
+    console.log(email);
+
+    e.target.reset();
+  }
+
   return (
     <div className="reg">
       <div className="regWrapper">
@@ -12,30 +32,38 @@ const SignUp = () => {
           </span>
         </div>
         <div className="regRight">
-          <form className="regBox">
+          <form className="regBox" onSubmit={submitHandler}>
             <input
               className="regInput"
               placeholder="Username"
               required
               autoFocus
+              value={username}
+              onChange={(e) => {setUsername(e.target.value)}}
             />
             <input
               className="regInput"
               placeholder="Email"
               type="email"
               required
+              value={email}
+              onChange={(e) => {setEmail(e.target.value)}}
             />
             <input
               className="regInput"
               placeholder="Password"
               type="password"
               required
+              value={password}
+              onChange={(e) => {setPassword(e.target.value)}}
             />
             <input
               className="regInput"
               placeholder="Password Confirmation"
               type="password"
               required
+              value={confirmPassword}
+              onChange={(e) => {setConfirmPassword(e.target.value)}}
             />
             <button className="regButton" type="submit">
               Create Account
